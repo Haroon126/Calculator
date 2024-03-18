18cc16dcd5932d209410cb16adf14a767a272dd6
[Haroon Memon] <[haroonmemon432@gmail.com]>
2024-03-18 23:25:29 +0500

    second commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([{
+    message : "enter your first Number" ,
+    type : "number",
+    name :  "firstNumber"
+    
+},
+{message : "enter your second Number" ,
+type : "number",
+name :  "secondNumber"
+
+},
+{message : "Select your choice what you want" ,
+type : "list",
+name : "operators",
+choices : ["addition","subraction","multiplication","division"]
+
+},
+])
+
+if (answer.operators === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber );
+    
+}else if (answer.operators === "subraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+}else if (answer.operators === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber)
+}else if (answer.operators === "division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+}else{
+    console.log("Please enter values");
+    
+}
